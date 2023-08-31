84c758b0caca676cdf38ac416a91148d0f50faa7
hwinkr <dnddl8280@naver.com>
2023-08-31 20:56:43 +0900

    🔧 config #3: axios 인스턴스 설정

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,1 +1,13 @@
-export {};
+import axios from 'axios';
+import GITHUB_BASE_URL from '../constants/base-url';
+
+const AxiosFetch = axios.create({
+  baseURL: GITHUB_BASE_URL,
+  headers: {
+    Authorization: `Bearer ${process.env.REACT_APP_GITHUB_API_KEY}`,
+    'Content-Type': 'application/json',
+    'X-GitHub-Api-Version': '2022-11-28',
+  },
+});
+
+export default AxiosFetch;
