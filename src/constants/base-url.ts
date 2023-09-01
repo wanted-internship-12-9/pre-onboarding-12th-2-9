209ed41e8a9b040324ee7da7c2688e7173688760
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-01 10:07:47 +0900

    🎨 style: organization, repository 상수처리

--- src/constants/base-url.ts
+++
@@ -1,3 +0,0 @@
-const GITHUB_BASE_URL = 'https://api.github.com';
-
-export default GITHUB_BASE_URL;
---
+++ src/constants/index.ts
@@ -0,0 +1,3 @@
+export const GITHUB_BASE_URL = 'https://api.github.com';
+export const ORGANIZATION_NAME = 'facebook';
+export const REPOSITORY_NAME = 'react';
