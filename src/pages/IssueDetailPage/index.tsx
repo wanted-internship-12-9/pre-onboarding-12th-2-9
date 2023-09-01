35fafc8a9bbb3d212c703ad8f5458a8cb8b0dea2
Eagerbeaver <86523545+YeongseoYoon@users.noreply.github.com>
2023-09-01 10:15:04 +0900

    ✨ feat: 디테일페이지 구현

--- src/pages/IssueDetailPage/index.tsx
+++ src/pages/IssueDetailPage/index.tsx
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import MarkdownPreview from '@uiw/react-markdown-preview';
 
-//import * as S from './IssueDetailPage.style';
+import { getIssueDetail } from '../../api/get-issues';
+import { IssueDetailItem } from '../../types/issues';
+import IssueHeader from '../../components/IssueHeader';
+import { ORGANIZATION_NAME, REPOSITORY_NAME } from '../../constants';
+
+import * as S from './IssueDetailPage.style';
 
 const IssueDetailPage = () => {
-  return <div>IssueDetailPage</div>;
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentPath = location.pathname;
+  const issueNumber = Number(currentPath.split('/')[2]);
+  const [issueDetail, setIssueDetail] = useState<IssueDetailItem>();
+
+  const getIssueDetailData = async () => {
+    try {
+      const IssueDetailData: IssueDetailItem = await getIssueDetail(
+        ORGANIZATION_NAME,
+        REPOSITORY_NAME,
+        issueNumber,
+      );
+      setIssueDetail(IssueDetailData);
+    } catch (error) {
+      navigate('/404');
+    }
+  };
+  useEffect(() => {
+    getIssueDetailData();
+  }, []);
+
+  return (
+    <S.IssueContainer>
+      {issueDetail && (
+        <>
+          <S.IssueHeaderContainer>
+            <IssueHeader issue={issueDetail} />
+          </S.IssueHeaderContainer>
+          <S.IssueBodyContainer>
+            <S.Profile src={issueDetail.user.avatar_url} alt="user image" />
+            <S.IssueBody>
+              <span>{issueDetail.user.login}</span>
+              <div>
+                <MarkdownPreview source={issueDetail.body} />
+              </div>
+            </S.IssueBody>
+          </S.IssueBodyContainer>
+        </>
+      )}
+    </S.IssueContainer>
+  );
 };
 
 export default IssueDetailPage;
